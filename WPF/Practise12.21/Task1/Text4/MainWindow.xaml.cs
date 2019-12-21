4b3ca6dd12b19e146954bbf6925d0307b78d641e
Tanya <tanyaryzhik33@gmail.com>
2019-12-21 14:29:47 +0200

    Some work

---
+++ WPF/Practise12.21/Task1/Text4/MainWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Text4
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            var person = new Person { FirstName = "Alen", LastName = "Martin", IsMaster = true };
+            this.DataContext = person;
+        }
+    }
+
+    public class Person
+    {
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public bool IsMaster { get; set; }
+    }
+}
