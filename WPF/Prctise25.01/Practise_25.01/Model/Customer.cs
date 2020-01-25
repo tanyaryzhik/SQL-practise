8b07721dcf6b37e2078d15c04609399007ebe954
Tanya <tanyaryzhik33@gmail.com>
2020-01-25 14:39:04 +0200

    Styles and triggers

---
+++ WPF/Prctise25.01/Practise_25.01/Model/Customer.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practise_25._01.Model
+{
+    public class Customer
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Country { get; set; }
+
+        public override string ToString()
+        {
+            return $"{FirstName} {LastName} {Country}";
+        }
+    }
+}
---
+++ WPF/Prctise25.01/Practise_25.01/ViewModel/MainWindowViewModel.cs
@@ -0,0 +1,24 @@
+﻿using Practise_25._01.Model;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practise_25._01.ViewModel
+{
+    public class MainWindowViewModel
+    {
+        public ObservableCollection<Customer> Customers { get; set; }
+        public Customer SelectedCustomer { get; set; }
+
+        public MainWindowViewModel()
+        {
+            this.Customers = new ObservableCollection<Customer> {
+                new Customer {FirstName = "Ivan", LastName = "Ivanoff", Country = "Russia" },
+            new Customer {FirstName = "Jason", LastName = "Smith", Country = "USA"  }};
+
+        }
+    }
+}
