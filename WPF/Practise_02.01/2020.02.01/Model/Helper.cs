2c945ea39bd8b04deae43ec24b7791fee5771d05
Tanya <tanyaryzhik33@gmail.com>
2020-02-01 14:16:52 +0200

    Test project, 01/02/2020

---
+++ WPF/Practise_02.01/2020.02.01/Model/Helper.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2020._02._01.Model
+{
+    public static class Helper
+    {
+        public static ObservableCollection<Car> GetCars()
+        {
+            return new ObservableCollection<Car>
+            {
+                new Car{Brand = "Fiat",
+                    Model="Doblo",
+                    ImagePath = @"C:\Users\user\source\SQL-practise\SQL-practise\WPF\Practise_02.01\2020.02.01\Model\Resources\fiat_doblo-pass.jpg",
+                    Price= 5000.00m,
+                    Color = "White",
+                    Country = "Italy",
+                    EngineType = "Diesel"},
+                new Car{Brand = "Renault",
+                    Model="Kangoo",
+                    ImagePath = @"C:\Users\user\source\SQL-practise\SQL-practise\WPF\Practise_02.01\2020.02.01\Model\Resources\renault_kangoo.jpg",
+                    Price= 5000.00m,
+                    Color = "Yellow",
+                    Country = "France",
+                    EngineType = "Diesel"},
+                new Car{Brand = "Renault",
+                    Model="Scenic",
+                    ImagePath = @"C:\Users\user\source\SQL-practise\SQL-practise\WPF\Practise_02.01\2020.02.01\Model\Resources\Renault-Scenic.jpg",
+                    Price= 5000.00m,
+                    Color = "Black",
+                    Country = "Italy", 
+                    EngineType = "Patrol"}
+            };
+        }
+
+        public static Car GetDefaultCar()
+        {
+            return new Car
+            {
+                Brand = "Default",
+                Model = "Default",
+                ImagePath = "Default",
+                Price = 1.00m,
+                Color = "Default",
+                Country = "Default",
+                EngineType = "Default"
+            };
+        }
+    }
+}
---
+++ WPF/Practise_02.01/2020.02.01/ViewModel/MainWindowViewModel.cs
@@ -0,0 +1,102 @@
+﻿using _2020._02._01.Model;
+using _2020._02._01.Model.Commands;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace _2020._02._01.ViewModel
+{
+    public class MainWindowViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private ObservableCollection<Car> cars;
+
+        public ObservableCollection<Car> Cars
+        {
+            get { return this.cars; }
+            set
+            {
+                if (this.cars == value)
+                    return;
+
+                this.cars = value;
+                this.OnPropertyChanged(nameof(this.Cars));
+            }
+        }
+
+        private Car selectedCar;
+        public Car SelectedCar
+        {
+            get { return this.selectedCar; }
+            set
+            {
+                if (this.selectedCar == value)
+                    return;
+
+                this.selectedCar = value;
+                this.OnPropertyChanged(nameof(this.SelectedCar));
+            }
+        }
+
+        private Car defaultCar;
+        public Car DefaultCar
+        {
+            get { return this.defaultCar; }
+            set
+            {
+                if (this.defaultCar == value)
+                    return;
+
+                this.defaultCar = value;
+                this.OnPropertyChanged(nameof(this.DefaultCar));
+            }
+        }
+
+        public ICommand RemoveCommand { get; set; }
+
+        public ICommand ResetCommand { get; set; }
+
+        public MainWindowViewModel()
+        {
+            this.Cars = Helper.GetCars();
+            this.DefaultCar = Helper.GetDefaultCar();
+            this.RemoveCommand = new RelayCommand(RemoveCommandExecute, GeneralCommandCanExecute);
+            this.ResetCommand = new RelayCommand(ResetCommandExecute, GeneralCommandCanExecute);
+        }
+
+        private bool GeneralCommandCanExecute(object obj)
+        {
+            return this.SelectedCar != null;
+        }
+
+        private void ResetCommandExecute(object obj)
+        {
+            this.SelectedCar.Brand = this.DefaultCar.Brand;
+            this.SelectedCar.Model = this.DefaultCar.Model;
+            this.SelectedCar.ImagePath = this.DefaultCar.ImagePath;
+            this.SelectedCar.Price = this.DefaultCar.Price;
+            this.SelectedCar.Country = this.DefaultCar.Country;
+            this.SelectedCar.Color = this.DefaultCar.Color;
+            this.SelectedCar.EngineType = this.DefaultCar.EngineType;
+        }
+
+        private void RemoveCommandExecute(object obj)
+        {
+            this.Cars.Remove(this.SelectedCar);
+        }
+
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            if (this.PropertyChanged is null)
+                return;
+
+            this.PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
